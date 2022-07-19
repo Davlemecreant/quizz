0d011dab4121a102a83ed48672d850004ed17e43
Davlemecreant <107454708+Davlemecreant@users.noreply.github.com>
2022-07-19 16:21:45 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,198 @@
+const form = document.querySelector('.form-quizz');
+let tableauResultats = [];
+const reponses = ['c','a','b','a','c','b','c','a','b','c','b','a','c','c','b','a','b','a','b','c'];
+// const emojis = ['✔️','✨','👀','😭','👎'];
+const titreResultat = document.querySelector('.resultats h2');
+const noteResultat = document.querySelector('.note');
+const aideResultat = document.querySelector('.aide');
+const toutesLesQuestions = document.querySelectorAll('.question-block');
+let verifTableau = [];
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // console.log(document.querySelector('input[name="q1"]:checked').value);
+
+    for(i = 1; i < 21; i++) {
+        tableauResultats.push(document.querySelector(`input[name="q${i}"]:checked`).value)
+    }
+    // console.log(tableauResultats);
+    verifFunc(tableauResultats);
+    tableauResultats = [];
+})
+
+function verifFunc(tabResultats) {
+
+    for(let a = 0; a < 20; a++){
+
+        if(tabResultats[a] === reponses[a]) {
+            verifTableau.push(true);
+        } else {
+            verifTableau.push(false);
+        }
+
+    }
+
+    // console.log(verifTableau);
+    afficherResultats(verifTableau);
+    couleursFonction(verifTableau);
+    verifTableau = [];
+}
+
+function afficherResultats(tabCheck) {
+
+    const nbDeFautes = tabCheck.filter(el => el !== true).length;
+    // console.log(nbDeFautes);
+
+    switch(nbDeFautes) {
+
+        case 0:
+            titreResultat.innerText = `✔️ Bravo, c'est un sans faute ! ✔️`
+            aideResultat.innerText = ''
+            noteResultat.innerText = '20/20'
+            break;
+        case 1:
+            titreResultat.innerText = `✨ Vous y êtes presque ! ✨`
+            aideResultat.innerText = 'Retentez une autre réponse dans la case rouge, puis re-validez !'
+            noteResultat.innerText = '19/20'
+            break;
+        case 2:
+            titreResultat.innerText = `✨ Vous y êtes presque ! ✨`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '18/20'
+            break;
+        case 3:
+            titreResultat.innerText = `✨ Vous y êtes presque ! ✨`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '17/20'
+            break;
+        case 4:
+            titreResultat.innerText = `✨ Vous y êtes presque ! ✨`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '16/20'
+            break;
+        case 5:
+            titreResultat.innerText = `✨ Vous y êtes presque ! ✨`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '15/20'
+        break;
+        case 6:
+            titreResultat.innerText = `✨ Encore un effort ... 👀`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '14/20'
+        break;
+        case 7:
+            titreResultat.innerText = `✨ Encore un effort ... 👀`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '13/20'
+        break;
+        case 8:
+            titreResultat.innerText = `✨ Encore un effort ... 👀`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '12/20'
+        break;
+        case 9:
+            titreResultat.innerText = `✨ Encore un effort ... 👀`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '11/20'
+        break;
+        case 10:
+            titreResultat.innerText = `✨ Encore un effort ... 👀`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '10/20'
+        break;
+        case 11:
+            titreResultat.innerText = `👀 Il reste quelques erreurs. 😭`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '9/20'
+        break;
+        case 12:
+            titreResultat.innerText = `👀 Il reste quelques erreurs. 😭`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '8/20'
+        break;
+        case 13:
+            titreResultat.innerText = `👀 Il reste quelques erreurs. 😭`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '7/20'
+        break;
+        case 14:
+            titreResultat.innerText = `👀 Il reste quelques erreurs. 😭`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '6/20'
+        break;
+        case 15:
+            titreResultat.innerText = `👀 Il reste quelques erreurs. 😭`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '5/20'
+        break;
+        case 16:
+            titreResultat.innerText = `😭 Peux mieux faire ! 😭`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '4/20'
+        break;
+        case 17:
+            titreResultat.innerText = `😭 Peux mieux faire ! 😭`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '3/20'
+        break;
+        case 18:
+            titreResultat.innerText = `😭 Peux mieux faire ! 😭`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '2/20'
+        break;
+        case 19:
+            titreResultat.innerText = `😭 Peux mieux faire ! 😭`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '1/20'
+        break;
+        case 20:
+            titreResultat.innerText = `👎 Peux mieux faire ! 👎`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '0/20'
+        break;
+        
+
+        default:
+            'Wops, cas innatendu.';
+
+    }
+
+}
+
+
+function couleursFonction(tabValBool) {
+
+    for(let j = 0; j < tabValBool.length; j++){
+
+        if(tabValBool[j] === true){
+            toutesLesQuestions[j].style.background = 'lightgreen';
+        } else {
+            toutesLesQuestions[j].style.background = '#ce1e18';
+            toutesLesQuestions[j].classList.add('echec');
+
+            setTimeout(() => {
+                toutesLesQuestions[j].classList.remove('echec');
+            }, 500)
+        }
+        
+    }
+
+}
+
+toutesLesQuestions.forEach(item => {
+    item.addEventListener('click', () => {
+        item.style.background = "#18ce73";
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
